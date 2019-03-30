f8fb802d3ad1ff24d1133a6ba71cca6554e83fde
ZhangYi <zhangyi@goldensecure.com>
2019-03-30 21:34:22 +0800

    finsh base function

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,27 @@
 use regex::Regex;
-use std::process::Command;
 use std::error::Error;
 use std::fmt;
+use std::net::Ipv4Addr;
+use std::process::Command;
 
-#[derive(PartialEq, Default, Clone, Debug)]
-struct Commit {
-    hash: String,
-    message: String,
+#[derive(PartialEq, Clone, Debug)]
+pub struct DefaultRoute {
+    gateway: Ipv4Addr,
+    dev: String,
+}
+
+impl DefaultRoute {
+    pub fn new(gateway: Ipv4Addr, dev: String) -> DefaultRoute {
+        DefaultRoute { gateway, dev }
+    }
+
+    pub fn gateway(&self) -> Ipv4Addr {
+        self.gateway.clone()
+    }
+
+    pub fn dev(&self) -> String {
+        self.dev.clone()
+    }
 }
 
 #[derive(Debug)]
@@ -24,28 +39,56 @@ impl Error for RunError {
     }
 }
 
-pub fn run() -> Result<(), Box<dyn Error>> {
-    let output = Command::new("git").arg("log").arg("--oneline").output()?;
+pub fn get_default_routes() -> Result<Vec<DefaultRoute>, Box<dyn Error>> {
+    let output = Command::new("ip").arg("route").arg("list").output()?;
 
     if !output.status.success() {
         return Err(Box::new(RunError {}));
     }
 
-    let pattern = Regex::new(r"(?x)
-                                ([0-9a-fA-f]+) # commit hash 
-                                (.*)           # The commit message")?;
-    
-    String::from_utf8(output.stdout)?
+    let pattern = Regex::new(r"(default\svia\s)(.*)(\sdev\s)([0-9a-zA-Z]*)(\s|\n)")?;
+
+    let routes = String::from_utf8(output.stdout)?
         .lines()
-        .filter_map(|line| pattern.captures(line))
-        .map(|cap| {
-            Commit {
-                hash: cap[1].to_string(),
-                message: cap[2].trim().to_string(),
-            }
-        })
-        .take(5)
-        .for_each(|x| println!("{:?}", x));
+        .filter_map(move |line| pattern.captures(line))
+        .map(|cap| DefaultRoute::new(cap[2].parse::<Ipv4Addr>().unwrap(), cap[4].to_string()))
+        .collect();
+
+    Ok(routes)
+}
+
+pub fn del_default_route(route: DefaultRoute) -> Result<(), Box<dyn Error>> {
+    let output = Command::new("ip")
+        .arg("route")
+        .arg("del")
+        .arg("default")
+        .arg("via")
+        .arg(route.gateway().to_string())
+        .arg("dev")
+        .arg(route.dev())
+        .output()?;
+
+    if !output.status.success() {
+        return Err(Box::new(RunError {}));
+    }
+
+    Ok(())
+}
+
+pub fn add_default_route(route: DefaultRoute) -> Result<(), Box<dyn Error>> {
+    let output = Command::new("ip")
+        .arg("route")
+        .arg("add")
+        .arg("default")
+        .arg("via")
+        .arg(route.gateway().to_string())
+        .arg("dev")
+        .arg(route.dev())
+        .output()?;
+
+    if !output.status.success() {
+        return Err(Box::new(RunError {}));
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use iproute::*;
+use std::env;
+use std::net::Ipv4Addr;
+
 fn main() {
-    iproute::run().unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        panic!("please input gateway and dev")
+    }
+
+    let gateway = args[1]
+        .parse::<Ipv4Addr>()
+        .expect("invalid gateway ip address");
+    let dev: String = args[2].clone();
+
+    let old_routes = get_default_routes().expect("get default routes failed");
+    println!("list default routes now:");
+    for route in &old_routes {
+        println!("{:?}", route);
+    }
+
+    let new_route = DefaultRoute::new(gateway, dev);
+    println!("add new default route {:?}", new_route);
+
+    // del old default routes
+    for route in old_routes {
+        del_default_route(route).expect("del default route failed");
+    }
+
+    // add new default route
+    add_default_route(new_route).expect("add new default route failed");
+
+    let new_routes = get_default_routes().expect("get default routes after add new route failed");
+    println!("now, the new default routes:");
+    for route in new_routes {
+        println!("{:?}", route);
+    }
 }
